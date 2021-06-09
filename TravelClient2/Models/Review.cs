77655c1f56aeb1ca68a2d5e34b564c0804306ec2
Ryan Walker <ryandanielwalker@gmail.com>
2021-06-09 10:47:58 -0700

    Upddate to include ReviewsController, GetDetails and Get methods
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

---
+++ TravelClient2/Models/Review.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TravelClient2.Models
+{
+  public class Review
+  {
+    public int ReviewId { get; set; }
+    public int Rating { get; set; }
+
+    public string Description { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+
+    public static List<Review> GetReviews()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Review> reviewList = JsonConvert.DeserializeObject<List<Review>>(jsonResponse.ToString());
+
+      return reviewList;
+    }
+    public static Review GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Review review = JsonConvert.DeserializeObject<Review>(jsonResponse.ToString());
+
+      return review;
+    }
+  }
+}
